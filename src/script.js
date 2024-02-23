9b715a8bb9d21fae51c382a630888d9759cf5a6d
heschofield <hayley@payableapps.com>
2024-02-23 13:49:54 -0500

    removed forecast HTML and added in JS

--- src/script.js
+++ src/script.js
@@ -92,3 +92,32 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="row">
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                class="weather-forecast-daily-icon"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max"> 18° </span>
+                <span class="weather-forecast-temperature-min">12° </span>
+              </div>
+            </div>
+          </div>
+          `;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
+displayForecast();
